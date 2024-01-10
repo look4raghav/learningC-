e165420b58bdc0a5c913d6d42d6cf6cc70a704f2
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2024-01-10 21:33:20 +0530

    Create right_triangle_pattern.c

---
+++ Practice/right_triangle_pattern.c
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+    printf("Hello world");
+
+    for (int i = 0; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("* ");
+        }printf(" \n");
+    }
+
+    return 0;
+}
