0d5353d98af2d828e59b761ab165539580699ed5
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2024-01-13 17:33:58 +0530

    Create typedef.c

---
+++ Practice/typedef.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+typedef int MYINT;
+
+int main() {
+    // Write C code here
+    
+    MYINT a = 15;
+    int y = 10;
+    
+    printf("Hello world\n %d\n %d\n", a, y);
+
+    return 0;
+}
