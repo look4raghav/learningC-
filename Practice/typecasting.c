b08670669fd0c82cbe4dfb7276be49645a3f1b8a
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2024-01-13 17:32:35 +0530

    Create typecasting.c

---
+++ Practice/typecasting.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    // Write C code here
+    float fl = 5.54;
+    char c = 10;
+    int integer;
+    int r;
+
+    integer = (int)(fl);
+    r = (int)c;
+    
+    printf("Hello world\n %d\n %d\n", integer, r);
+
+    return 0;
+}
