4de056cf6c4ffbeaf202525339103a044901cbac
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2024-01-13 17:33:17 +0530

    Create const-variable.c

---
+++ Practice/const-variable.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+int main() {
+    // Write C code here
+    const int a = 15;
+    
+    printf("%d\n", a);
+
+    return 0;
+}
+
+
+
+
+// In C, preprocessors are directives that are processed before the compilation of the actual source code. They begin with the # symbol and are processed by the preprocessor, which is a separate step in the compilation process. Preprocessors are used to manipulate the source code before it is passed to the compiler.
