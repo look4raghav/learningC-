e7fe0d5cbe2682ede2981e4a1a1491e694692f14
RAGHAV AGGARWAL <88137758+look4raghav@users.noreply.github.com>
2024-01-14 10:23:02 +0530

    Create add_using_bitwise.c

---
+++ Practice/add_using_bitwise.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int addWithoutOperator(int a, int b) {
+    while (b != 0) {
+        int carry = a & b;
+
+        a = a ^ b;
+
+        b = carry << 1;
+    }
+
+    return a;
+}
+
+int main() {
+    int num1, num2;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    int sum = addWithoutOperator(num1, num2);
+
+    printf("Sum: %d\n", sum);
+
+    return 0;
+}
